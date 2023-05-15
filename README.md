d4f403afbad815bb7294453c4c52a6155389c151
zrebec <zrebec@zrebec.sk>
2023-05-15 19:08:41 +0200

    feat: variables refactor

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # MathForKids
-This is a simple math exercise program that is designed to help kids practice addition and subtraction. The program generates two random numbers, num1 and num2, between 0 and 100, and asks the user to perform either an addition or subtraction operation, depending on the result of the random boolean isAddition.
+This is a simple math exercise program that is designed to help kids practice addition and subtraction. The program generates two random numbers, `operand1` and `operand2`, between 0 and 100, and asks the user to perform either an addition or subtraction operation, depending on the result of the random boolean `isAddition`.
 
 ## How it works
-The program starts by setting the initial variables: the maximum allowed time for an answer in seconds (maxAllowedTimeInSeconds), the expected number of exercises (expectedCalculatedExercises), and the boolean isAddition to determine whether the operation will be an addition or subtraction.
+The program starts by setting the initial variables: the maximum allowed time for an answer in seconds (`maxAllowedTimeInSeconds`), the expected number of exercises (`expectedCalculatedExercises`), and the boolean `isAddition` to determine whether the operation will be an addition or subtraction.
 
-In each cycle, the program generates a random number num1 between 0 and 100. Then it calculates num2 based on the isAddition value: If it is true, num2 will be a random number from 0 to the difference of 101 and num1 (to ensure the sum does not exceed 100); if isAddition is false, num2 will be a random number from 0 to num1 (to ensure the result of the subtraction does not go below 0).
+In each cycle, the program generates a random number `operand1` between 0 and 100. Then it calculates `operand2` based on the isAddition value: If it is true, `operand2` will be a random number from 0 to the difference of 101 and `operand1` (to ensure the sum does not exceed 100); if isAddition is false, `operand2` will be a random number from 0 to `operand1` (to ensure the result of the subtraction does not go below 0).
 
 The program then prints the generated operation (either addition or subtraction) and waits for the user's input. If the user's answer is correct and within the maximum allowed time, the program will print a congratulatory message. If the user's answer is incorrect or if the user takes more time than allowed, the program will print the correct result and either a notification about the incorrect answer or the time limit exceeding.
 
-The program continues to generate exercises until the number of successful inputs reaches expectedCalculatedExercises. After that, it prints a summary including the number of successful exercises, the total number of exercises, the total time taken, and the success ratio.
+The program continues to generate exercises until the number of successful inputs reaches `expectedCalculatedExercises`. After that, it prints a summary including the number of successful exercises, the total number of exercises, the total time taken, and the success ratio.
 
 ## Usage
 To use this program, simply run it in your Java environment. When prompted with an operation, type in your answer and press Enter. If your answer is correct and entered within the allotted time, you will receive a congratulatory message. If your answer is incorrect or if you exceed the time limit, you will receive the correct result and a message about your error or delay.
--- src/sk/zrebec/java/learning/basics/MathForKids.java
+++ src/sk/zrebec/java/learning/basics/MathForKids.java
@@ -7,54 +7,54 @@ import java.util.concurrent.TimeUnit;
 public class MathForKids {
     public static void main(String[] args) {
         Random random = new Random();
-        int num2, num1, sum;
+        int operand1, operand2, expectedAnswer;
 
         // Set initial variables
         final int maxAllowedTimeInSeconds = 12;
         final int expectedCalculatedExercises = 6;
         boolean isAddition;
-        int successInputs = 0;
-        int totalInputs = 0;
-        int totalSeconds = 0;
+        int successfulExercisesCount = 0;
+        int totalAttemptedExercisesCount = 0;
+        int totalTimeInSeconds = 0;
 
         do try {
             /*
              * Now we can set some variables at start of every cycle
              *
              * @isAddition is true of false based on random
-             * @num1 will be always random number between 0 and 100
-             * @num2 will be calculated based on addition. If added, we need to subtract
+             * @operand1 will be always random number between 0 and 100
+             * @operand2 will be calculated based on addition. If added, we need to subtract
              * number1 from 101
-             * @sum sum is calculated also on addition condition. If is true, the sum is
-             * num1 + number, otherwise (we mean minus, then num1 - num2)
-             * @totalInputs after every input we must add input to "totalInputs" variable
+             * @expectedAnswer expectedAnswer is calculated also on addition condition. If is true, the expectedAnswer is
+             * operand1 + number, otherwise (we mean minus, then operand1 - operand2)
+             * @totalAttemptedExercisesCount after every input we must add input to "totalAttemptedExercisesCount" variable
              */
             isAddition = random.nextBoolean();
-            num1 = random.nextInt(101);
-            num2 = random.nextInt(isAddition ? 101 - num1 : num1);
-            sum = isAddition ? (num1 + num2) : (num1 - num2);
-            System.out.printf("%d %s %d\n", num1, isAddition ? "+" : "-", num2);
+            operand1 = random.nextInt(101);
+            operand2 = random.nextInt(isAddition ? 101 - operand1 : operand1);
+            expectedAnswer = isAddition ? (operand1 + operand2) : (operand1 - operand2);
+            System.out.printf("%d %s %d\n", operand1, isAddition ? "+" : "-", operand2);
             Scanner scanner = new Scanner(System.in);
-            totalInputs++;
+            totalAttemptedExercisesCount++;
             long start = System.nanoTime();
             int result = scanner.nextInt();
             long duration = TimeUnit.SECONDS.convert(System.nanoTime() - start, TimeUnit.NANOSECONDS);
-            if (result == sum && duration <= maxAllowedTimeInSeconds) {
-                System.out.printf("This is OK. The real sum is %d\n", sum);
-            } else if (result != sum && duration <= maxAllowedTimeInSeconds) {
+            if (result == expectedAnswer && duration <= maxAllowedTimeInSeconds) {
+                System.out.printf("This is OK. The real expectedAnswer is %d\n", expectedAnswer);
+            } else if (result != expectedAnswer && duration <= maxAllowedTimeInSeconds) {
                 // Student's answer was right and in max allowed time
-                System.out.printf("You're fast. But you have incorrect result. Correct result is %d\n", sum);
-            } else if (result == sum) {
+                System.out.printf("You're fast. But you have incorrect result. Correct result is %d\n", expectedAnswer);
+            } else if (result == expectedAnswer) {
                 // Student's answer was right but too slow
                 System.out.printf("You're on good way. Correct result but your time was %ds\n", duration);
             } else {
                 // Student's answer was incorrect
-                System.out.printf("Incorrect. Correct result is %d and maximum duration is %ds\n", sum, maxAllowedTimeInSeconds);
+                System.out.printf("Incorrect. Correct result is %d and maximum duration is %ds\n", expectedAnswer, maxAllowedTimeInSeconds);
             }
-            successInputs++;
+            successfulExercisesCount++;
         } catch (Exception e) {
             System.out.println("This is not number");
-        } while (successInputs < expectedCalculatedExercises);
+        } while (successfulExercisesCount < expectedCalculatedExercises);
 
         System.out.printf(
                 """
@@ -63,11 +63,11 @@ public class MathForKids {
                         Your total time was %d seconds and
                         Your success ratio was %.2f%%.
                 """,
-                successInputs,
+                successfulExercisesCount,
                 maxAllowedTimeInSeconds,
-                totalInputs,
-                totalSeconds,
-                ((double) successInputs / totalInputs) * 100);
+                totalAttemptedExercisesCount,
+                totalTimeInSeconds,
+                ((double) successfulExercisesCount / totalAttemptedExercisesCount) * 100);
     }
 
 
